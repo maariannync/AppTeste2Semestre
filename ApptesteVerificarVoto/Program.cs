b838ae8d42dfa947bfeb04019505c7e58237adc5
marianny <mariannync22@gmail.com>
2024-08-01 15:06:07 -0300

    Verificar voto

---
+++ ApptesteVerificarVoto/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ApptesteVerificarVoto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a sua idade:");
+            int idade = int.Parse(Console.ReadLine());
+
+            if (idade < 16)
+            {
+                Console.WriteLine("Não pode votar.");
+            }
+            else if (idade >= 16 && idade < 18)
+            {
+                Console.WriteLine("Voto opcional.");
+            }
+            else
+            {
+                Console.WriteLine("Pode votar.");
+            }
+        }
+    }
+}
+
